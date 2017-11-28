bb1cef3278259337cd0d92a7924f57935ec82d3d
bai3 <2442694004@qq.com>
2017-11-28 16:15:29 +0800

    :lipstick: 更新登录注册页面

--- index/admin.py
+++ index/admin.py
@@ -5,10 +5,12 @@ from .models import Article, Work
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'tag', 'type', 'pub_time')
+    search_fields = ('title',)
 
 
 class ShowAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_time', 'url', 'img_url')
+    search_fields = ('title',)
 
 admin.site.register(Article, PostAdmin)
 admin.site.register(Work, ShowAdmin)
